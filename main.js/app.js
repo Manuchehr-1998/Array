3c36d752b147e99401186afafb8df17344520e0b
Manuchehr-1998 <120194005+Manuchehr-1998@users.noreply.github.com>
2022-12-30 15:24:52 +0500

    sadasd

--- main.js/app.js
+++ main.js/app.js
@@ -1,5 +1,5 @@
-let countries = ["Tapan", "Smerika", "Nenesuela"];
+let countries = ["Oapan", "Amerika", "Venesuela"];
 
 alert(countries.sort((a, b) => (a > b ? 1 : -1))); // Andorra, Vietnam, Österreich (неправильно)
 
-alert(countries.sort((a, b) => a.localeCompare(b))); // Amerika,Benesuela,Japan (правильно!)
+// alert(countries.sort((a, b) => a.localeCompare(b))); // Amerika,Benesuela,Japan (правильно!)
