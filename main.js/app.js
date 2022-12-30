6def5de3735adb5db9ac6c3e715f6b4840ea6377
Manuchehr-1998 <120194005+Manuchehr-1998@users.noreply.github.com>
2022-12-30 14:12:55 +0500

    asss

--- main.js/app.js
+++ main.js/app.js
@@ -1,3 +1,5 @@
-["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
-  alert(`${item}  ${index} ${array}`);
-});
+let countries = ["Tapan", "Smerika", "Nenesuela"];
+
+alert(countries.sort((a, b) => (a > b ? 1 : -1))); // Andorra, Vietnam, Österreich (неправильно)
+
+alert(countries.sort((a, b) => a.localeCompare(b))); // Amerika,Benesuela,Japan (правильно!)
