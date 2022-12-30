3c0d2bd641f27ff7539268139152224a9b8ce9e0
Manuchehr-1998 <120194005+Manuchehr-1998@users.noreply.github.com>
2022-12-30 15:45:41 +0500

    asdsa

--- main.js/app.js
+++ main.js/app.js
@@ -1,5 +1,27 @@
-let countries = ["Oapan", "Amerika", "Venesuela"];
+// let countries = ["Oapan", "Amerika", "Venesuela"];
 
-alert(countries.sort((a, b) => (a > b ? 1 : -1))); // Andorra, Vietnam, Österreich (неправильно)
+// alert(countries.sort((a, b) => (a > b ? 1 : -1))); // Andorra, Vietnam, Österreich (неправильно)
 
 // alert(countries.sort((a, b) => a.localeCompare(b))); // Amerika,Benesuela,Japan (правильно!)
+// let users = [
+//   { id: 1, name: "Вася" },
+//   { id: 2, name: "Петя" },
+//   { id: 3, name: "Маша" },
+// ];
+
+// // возвращает массив, состоящий из двух первых пользователей
+// let someUsers = users.filter((item) => item.id < 3);
+
+// alert(someUsers.length); // 2
+// let arr = [1, 2, 15];
+
+// arr.sort(function (a, b) {
+//   return a - b;
+// });
+
+// alert(arr); // 1, 2, 15
+
+let names = "Вася, Петя, Маша";
+
+let arr = names.split(",");
+alert(arr);
